a4ddcebaa291afc6c72fb3b33c9692da1ad75f24
davidthegood <34786497+davidthegood@users.noreply.github.com>
2019-02-07 00:48:15 -0600

    Moved Header and footer to respective js files.

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+function makeFooter(){
+    var footerHTML =
+    "<div class=\"row\">"+
+      "<div class=\"col-xl-12\">"+
+        "<footer>"+
+            "<p>powered by GitHub Pages</p>"+
+        "</footer>"+
+      "</div>"+
+    "</div>";
+
+    $('#content').after(footerHTML);
+}
---
+++ js/header.js
@@ -0,0 +1,43 @@
+function makeHeader(){
+    var headerHTML = "<div class=\"container-fluid\">"+
+      "<div class=\"row\"\>"+
+        "<div class=\"col-xl-12\">"+
+          "<header>"+
+              "<center>"+
+                "<img class=\"img-fluid\" src=\"https://docs.google.com/drawings/d/e/2PACX-1vTMmMei0LpZNDdaPfdmn0VUnilF6pxfwbPzDAP5D0EKA6EBaXxth5oKoA0eu9-_62W5JW0OfYqGOozP/pub?w=985&amp;h=184\">"+
+              "</center>"+
+          "</header>"+
+        "</div>"+
+      "</div>"+
+
+      "<div class=\"row\">"+
+        "<div class=\"col-xl-12\">"+
+          "<nav class=\"navbar navbar-expand-xl bg-dark navbar-dark\">"+
+
+            "<a href=\"#\" class=\"navbar-brand\">David Moore</a>"+
+
+            "<button class=\"navbar-toggler\"type=\"button\"data-toggle=\"collapse\"data-target=\"#collapsemenu\"aria-expanded=\"false\">"+
+
+                "<span class=\"navbar-toggler-icon\"></span>"+
+            "</button>"+
+
+            "<div class=\"collapse navbar-collapse\" id=\"collapsemenu\">"+
+                "<ul class=\"navbar-nav ml-auto\">"+
+                    "<li class=\"nav-item\">"+
+                        "<a href=\"index.html\" class=\"nav-link active\">Home</a>"+
+                    "</li>"+
+                    "<li class=\"nav-item\">"+
+                        "<a href=\"#\" class=\"nav-link\">About</a>"+
+                    "</li>"+
+                    "<li class=\"nav-item\">"+
+                        "<a href=\"#\" class=\"nav-link\">Form</a>"+
+                    "</li>"+
+                "</ul>"+
+            "</div>"+
+
+          "</nav>"+
+        "</div>"+
+      "</div>";
+
+      $('body').append(headerHTML);
+}
